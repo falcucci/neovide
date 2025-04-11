a4090aa1830cb41c37d42bb7487ee3a7e1ecd7bc
Alexsander Falcucci <alex.falcucci@gmail.com>
2025-04-11 23:35:52 +0200

    refactor: refactor terminal settings for neovim
    - remove setting of `vim.o.number`
    - adjust neovide terminal settings to include `termguicolors`, nonumber, norelativenumber, and better autocmd handling

--- lua/term.lua
+++ lua/term.lua
@@ -1,22 +1,12 @@
 if vim.g.neovide_terminal_mode then
   vim.o.laststatus = 0
   vim.o.cmdheight = 0
-  vim.o.number = false
 
   vim.g.neovide_padding_left = 0
-
   vim.cmd([[
     set termguicolors
-    autocmd TermOpen * setlocal nonumber norelativenumber
-    autocmd VimEnter * terminal | startinsert
     terminal
-    set nu!
-    autocmd TermOpen * setlocal nonumber norelativenumber
-    autocmd VimEnter * terminal | startinsert
     startinsert
-    autocmd TermOpen * setlocal nonumber norelativenumber
-    autocmd VimEnter * terminal | startinsert
-    setlocal nonumber
     autocmd BufLeave term://* quit
 ]])
 end
